0eb8bbbfdfbce2647982d0255855ca138f667712
Ana-Vega05 <143562484+Ana-Vega05@users.noreply.github.com>
2025-11-04 13:31:12 -0500

    fix: Deployment Adjustment

--- app/core/config.py
+++ app/core/config.py
@@ -22,7 +22,7 @@ class Settings(BaseSettings):
     REFRESH_TOKEN_EXPIRE_DAYS: int = 1
     
     # ==================== FIREBASE ====================
-    FIREBASE_CREDENTIALS_PATH: str = "serviceAccountKey.json"
+    FIREBASE_CREDENTIALS_PATH: str = "firebase_credentials.json"
     FIREBASE_DATABASE_URL: Optional[str] = None
     FIREBASE_API_KEY: str = "AIzaSyCbEN3vnK6AtsLNbaKPPMT4Iz0hrP5vSuk"
     
